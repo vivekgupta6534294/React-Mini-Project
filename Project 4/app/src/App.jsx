4be4224426345fe17ba26095ce7c8dc71b5fc740
Vivek Gupta <Vivekgupta6534294@gmail.com>
2024-08-15 22:06:23 +0530

    Top Container Started

--- Project 4/app/src/App.jsx
+++ Project 4/app/src/App.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components"
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <MainContainer>
+      <TopContainer>
+        <div>
+          <img src="/logo.svg" alt="" />
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search Food "/>
+        </div>
+        
+
+      </TopContainer>
+    </MainContainer>
+  );
 };
 
 export default App;
+
+const MainContainer = styled.div`
+background-color: #323334`;
+const TopContainer =styled.section``;
